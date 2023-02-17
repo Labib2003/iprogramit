a49e6618df961d486e418c9de6fa843e5ca549a4
Labib2003 <labib.salimi30@gmail.com>
2023-02-17 11:09:28 +0600

    routes updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1/job", jobRoutes);
+app.use("/api/v1/jobs", jobRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
